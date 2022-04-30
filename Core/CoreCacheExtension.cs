893f7d4b93178e88daab61b09857f1691535f425
Anson.G <Anson.G@sjfood.us>
2022-04-30 06:44:08 -0700

    Chore: Update README

--- Core/CoreCacheExtension.cs
+++ Core/CoreCacheExtension.cs
@@ -1,13 +1,20 @@
 using Core.Driver;
 using Core.Entity;
+using Core.Enum;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Core;
 
 public static class CoreCacheExtension
 {
-    public static void AddCoreCache(this IServiceCollection services, uint buckets = 5)
+    public static void AddCoreCache(
+        this IServiceCollection services,
+        uint buckets = 5,
+        uint bucketMaxCapacity = 500000,
+        MaxMemoryPolicy maxMemoryPolicy = MaxMemoryPolicy.LRU,
+        int cleanUpPercentage = 10
+    )
     {
-        services.AddSingleton<ICacheClient>(new MemoryCache(buckets));
+        services.AddSingleton<ICacheClient>(new MemoryCache(buckets, bucketMaxCapacity, MaxMemoryPolicy.LRU, cleanUpPercentage));
     }
 }
